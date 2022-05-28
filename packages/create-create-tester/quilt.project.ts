6dbfbf40d0f59b09eacf2b7515a0de0bbdaa97a2
Chris Sauve <chrismsauve@gmail.com>
2022-05-28 11:21:12 -0400

    Fix root TSConfig and type errors

--- packages/create-create-tester/quilt.project.ts
+++ packages/create-create-tester/quilt.project.ts
@@ -1,6 +1,6 @@
 import {createPackage, quiltPackage} from '@quilted/craft';
 
 export default createPackage((pkg) => {
-  pkg.binary({name: 'create-tester', entry: './source/index.ts'});
+  pkg.binary({name: 'create-tester', source: './source/index.ts'});
   pkg.use(quiltPackage());
 });
