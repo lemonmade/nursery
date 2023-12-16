d521e91723a6be145fedbb53274ab870ac759530
Chris Sauve <chrismsauve@gmail.com>
2023-12-16 01:18:29 -0500

    Cleanup

--- packages/remote-ui-preact/develop/vite.config.js
+++
@@ -1,6 +0,0 @@
-import {defineConfig} from 'vite';
-import {quiltPackage} from '@quilted/craft/vite';
-
-export default defineConfig({
-  plugins: [quiltPackage()],
-});
--- packages/remote-ui-preact/vite.config.js
+++ packages/remote-ui-preact/vite.config.js
@@ -2,13 +2,5 @@ import {defineConfig} from 'vite';
 import {quiltPackage} from '@quilted/craft/vite';
 
 export default defineConfig({
-  esbuild: {
-    tsconfigRaw: {
-      compilerOptions: {
-        experimentalDecorators: true,
-        emitDecoratorMetadata: true,
-      },
-    },
-  },
   plugins: [quiltPackage()],
 });
