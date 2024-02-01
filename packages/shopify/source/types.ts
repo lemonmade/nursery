fa1c1498e8e068c47781b412854b4573d7fc8b81
Chris Sauve <chrismsauve@gmail.com>
2024-02-01 08:23:20 +0000

    fix lint

--- packages/shopify/source/types.ts
+++ packages/shopify/source/types.ts
@@ -2,11 +2,7 @@ export type UnstableAPIVersion = 'unstable';
 
 export type ReleaseCandidateAPIVersion = '2024-04';
 
-export type SupportedAPIVersion =
-  | '2024-01'
-  | '2023-10'
-  | '2023-07'
-  | '2023-04';
+export type SupportedAPIVersion = '2024-01' | '2023-10' | '2023-07' | '2023-04';
 
 export type APIVersion =
   | UnstableAPIVersion
